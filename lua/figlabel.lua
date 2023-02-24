b61d9bb55e221840b350c1c43f9bd6204acacddf
Guy Mackenzie <guy.mackenzie@macfiver.com>
2023-02-24 07:26:55 -0600

    changed to just use lua file and added vim help file

--- lua/figlabel.lua
+++ lua/figlabel.lua
@@ -1,25 +1,53 @@
-local function figlabel(text, prefix)
-  if text == "" then
-    put(text)
-    print("error: no text supplied")
-    return
-  end
-  if prefix == "" then
-    put(prefix)
-    print("error: no prefix supplied")
-    return
-  end
+--   __ _       _       _          _         _             _
+--  / _(_) __ _| | __ _| |__   ___| |  _ __ | |_   _  __ _(_)_ __
+-- | |_| |/ _` | |/ _` | '_ \ / _ \ | | '_ \| | | | |/ _` | | '_ \
+-- |  _| | (_| | | (_| | |_) |  __/ | | |_) | | |_| | (_| | | | | |
+-- |_| |_|\__, |_|\__,_|_.__/ \___|_| | .__/|_|\__,_|\__, |_|_| |_|
+--        |___/                       |_|            |___/
 
+---@mod figlabel Figlabel
+---@brief [[
+---Figlabel appends a figlet created label after the current line.
+---
+--- Usage:
+---    :Figlabel <label text> <comment prefix>
+---
+---    label text:     text string - escape spaces with a '\'
+---                    for `\` use '\\'
+---    comment prefix: comment prefix for language
+---@brief ]]
+local function flabel(text, prefix)
   local fn = vim.fn
+  -- get figlet output into a list
   local result = fn.systemlist('figlet '..text)
+  -- get current line # in current buffer
   local linenr = fn.line('.',fn.bufwinid(fn.bufnr()))
-  for k,v in pairs(result) do
+  -- remove all trailing whitspace from each list line
+  for k, _ in pairs(result) do
     local txt = prefix..' '..result[k]
     result[k] = fn.substitute(txt, " *$", "", "")
   end
+  -- append figlet list after current line
   fn.append(linenr, result)
 end
 
-return {
-  figlabel = figlabel
-}
+-- create 'Figlabel' Ex command
+vim.api.nvim_create_user_command("Figlabel",
+  function(input)
+    local text, prefix
+    local args = input.fargs
+    if #args == 0 then
+      print("error: no text argument supplied")
+      return
+    elseif #args == 1 then
+      print("error: no prefix argument supplied")
+      return
+    else
+      text = args[1]
+      prefix = args[2]
+    end
+    flabel(text, prefix)
+  end,
+  {nargs = "+", desc = "create a figlet label", force = true})
+
+---@export figlabel
