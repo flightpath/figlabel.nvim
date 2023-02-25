82d193dabeecdbd176b729d1bbf0e774b43dd874
Guy Mackenzie <guy.mackenzie@macfiver.com>
2023-02-25 03:59:24 -0600

    updated to use module and setup

--- lua/figlabel.lua
+++ lua/figlabel.lua
@@ -5,18 +5,9 @@
 -- |_| |_|\__, |_|\__,_|_.__/ \___|_| | .__/|_|\__,_|\__, |_|_| |_|
 --        |___/                       |_|            |___/
 
----@mod figlabel Figlabel
----@brief [[
----Figlabel appends a figlet created label after the current line.
----
---- Usage:
----    :Figlabel <label text> <comment prefix>
----
----    label text:     text string - escape spaces with a '\'
----                    for `\` use '\\'
----    comment prefix: comment prefix for language
----@brief ]]
-local function flabel(text, prefix)
+local M = {}
+
+M.flabel = function (text, prefix)
   local fn = vim.fn
   -- get figlet output into a list
   local result = fn.systemlist('figlet '..text)
@@ -32,22 +23,24 @@ local function flabel(text, prefix)
 end
 
 -- create 'Figlabel' Ex command
-vim.api.nvim_create_user_command("Figlabel",
-  function(input)
-    local text, prefix
-    local args = input.fargs
-    if #args == 0 then
-      print("error: no text argument supplied")
-      return
-    elseif #args == 1 then
-      print("error: no prefix argument supplied")
-      return
-    else
-      text = args[1]
-      prefix = args[2]
-    end
-    flabel(text, prefix)
-  end,
-  {nargs = "+", desc = "create a figlet label", force = true})
+M.setup = function ()
+  vim.api.nvim_create_user_command("Figlabel",
+    function(input)
+      local text, prefix
+      local args = input.fargs
+      if #args == 0 then
+        print("error: no text argument supplied")
+        return
+      elseif #args == 1 then
+        print("error: no prefix argument supplied")
+        return
+      else
+        text = args[1]
+        prefix = args[2]
+      end
+      require'figlabel'.flabel(text, prefix)
+    end,
+    {nargs = "+", desc = "create a figlet label", force = true})
+end
 
----@export figlabel
+return M
